22dc1525d7efd8549b841515847e0a7391e12d06
Windy435 <tnphong435@gmail.com>
2016-05-19 19:30:21 +0700

    Add entity framework codefirst for data

---
+++ StupigShop/StupigShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupigShop.Data.Infrastructure
+{
+    public class DbFactory : Dispoable, IDbFactory
+    {
+        private StupigShopDbContext dbContext;
+        public StupigShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new StupigShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if(dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ StupigShop/StupigShop.Data/StupigShopDbContext.cs
@@ -0,0 +1,35 @@
+﻿using StupigShop.Model.Models;
+using System.Data.Entity;
+
+namespace StupigShop.Data
+{
+    public class StupigShopDbContext : DbContext
+    {
+        public StupigShopDbContext() : base("StupigShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Footer> Footers { set; get; }
+        public DbSet<Menu> Menus { set; get; }
+        public DbSet<MenuGroup> MenuGroups { set; get; }
+        public DbSet<Order> Orders { set; get; }
+        public DbSet<OrderDetail> OrderDetails { set; get; }
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Post> Posts { set; get; }
+        public DbSet<PostCategory> PostCategories { set; get; }
+        public DbSet<PostTag> PostTags { set; get; }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<ProductCategory> ProductCategorys { set; get; }
+        public DbSet<ProductTag> ProductTags { set; get; }
+        public DbSet<Slide> Slides { set; get; }
+        public DbSet<SupportOnline> SupportOnlines { set; get; }
+        public DbSet<SystemConfig> SystemConfigs { set; get; }
+        public DbSet<Tag> Tags { set; get; }
+        public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
