d877c09283433d87150b71789f310e45fbfd0bd9
Windy435 <tnphong435@gmail.com>
2016-06-06 01:02:52 +0700

    Add filter

---
+++ StupigShop/StupigShop.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function(){
+        return function(input){
+            if(input == true)
+                return 'Enable';
+            else
+                return 'Disable';
+        }
+    });
+})(angular.module('stupigshop.common'));
