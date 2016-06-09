f1d12da5d42919208e10d1ef693a394907696d01
Windy435 <tnphong435@gmail.com>
2016-06-09 14:45:25 +0700

    Setup routing cofig

--- StupigShop/StupigShop.Web/App_Start/RouteConfig.cs
+++ StupigShop/StupigShop.Web/App_Start/RouteConfig.cs
@@ -10,10 +10,38 @@ namespace StupigShop.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "login.html",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+                namespaces: new string[] { "StupigShop.Web.Models" }
+            );
+
+            routes.MapRoute(
+                name: "About",
+                url: "about.html",
+                defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "StupigShop.Web.Models" }
+            );
+
+            routes.MapRoute(
+                name: "Product Category",
+                url: "{alias}.pc-{id}.html",
+                defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+                namespaces: new string[] { "StupigShop.Web.Models" }
+            );
+
+            routes.MapRoute(
+                name: "Product",
+                url: "{alias}.p-{id}.html",
+                defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
+                namespaces: new string[] { "StupigShop.Web.Models" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "StupigShop.Web.Models" }
             );
         }
     }
